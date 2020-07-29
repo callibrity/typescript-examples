ce402dac8dbc5aff7c7bf4aaece244e92cbd3984
Alex Morelli <alexmorelli@CVG-C02RK01YG8WQ.local>
2020-07-29 11:47:41 -0400

    removed packages and files that aren't needed

---
+++ src/9-strictness-settings.tsx
@@ -0,0 +1,8 @@
+// the "strictness" level of TypeScript can be adjusted using various config settings
+// the main settings, which are also in the tsconfig.json of this project, are below
+
+// "noImplicitAny": true, // Forces explicit type definition anywhere TypeScript can't assume what the type will be
+// "allowJs": true, // Restricts from using .js and .jsx files
+// "strictNullChecks": false, // Prevents referencing null or undefined values
+// "noImplicitReturns": false, // Requires all code paths to return a value
+// "allowSyntheticDefaultImports": true, // Prevents default imports from modules that don't have default exports
